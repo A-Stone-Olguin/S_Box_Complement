efe9700d2dbccaede40f210a5f8b2877e6355502
Stone Olguin <olguinstone@gmail.com>
2023-10-27 13:53:17 -0600

    Writes Results to files now

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def two_by_two_s_box_unknown():
         sys.stdout.flush()
     return
 
-def n_by_n_s_box_unknown(n):
+def n_by_n_s_box_unknown(n, file_object):
     elements = n**2
     bits = ceil(log2(elements))
     for weight in range(2, bits+1):
@@ -151,18 +151,20 @@ def n_by_n_s_box_unknown(n):
                 s.add(Hamming_Distance(known_bvs[i], unknown_bvs[i]) == distance)
 
             if(s.check() == sat):
-                print("Constraints Satisified for HW: %d and HD: %d" %(weight, distance)),
+                print("Constraints Satisified for HW: %d and HD: %d" %(weight, distance), file=file_object),
                 m = s.model()
                 for d in m.decls():
-                    print('\t{state}\t->\t{encode:0{fieldsize}b}'.format(state=d.name(),encode=m[d].as_long(),fieldsize=bits))
+                    print('\t{state}\t->\t{encode:0{fieldsize}b}'.format(state=d.name(),encode=m[d].as_long(),fieldsize=bits), file=file_object)
             else:
-                print("Constraints Not Satisified for HW: %d and HD: %d" %(weight, distance))
+                print("Constraints Not Satisified for HW: %d and HD: %d" %(weight, distance), file=file_object)
                 sys.stdout.flush()
     return
 
 def main():
     for n in range(2, 17):
-        n_by_n_s_box_unknown(n)
+        filename = f"{n}_by_{n}_S_box.txt"
+        with open(filename, "w") as f:
+            n_by_n_s_box_unknown(n, f)
     return
 
 if __name__ == "__main__":
