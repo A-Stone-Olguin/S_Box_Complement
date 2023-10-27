7223723fe91cef6ec1db32400229e5c20a7b73fd
Stone Olguin <olguinstone@gmail.com>
2023-10-27 13:47:51 -0600

    Works for generalized distance and weight.

--- main.py
+++ main.py
@@ -123,44 +123,46 @@ def two_by_two_s_box_unknown():
         sys.stdout.flush()
     return
 
-def n_by_n_s_box_unknown(n, weight, distance):
+def n_by_n_s_box_unknown(n):
     elements = n**2
     bits = ceil(log2(elements))
+    for weight in range(2, bits+1):
+        for distance in range(2, bits+1):
+            unknown_bvs = []
+            for i in range(elements):
+                name = "unknown_bv_{row}_{col}".format(row=i%n, col = floor(i/n))
+                bv = BitVec(name, bits)
+                unknown_bvs.append(bv)
 
-    unknown_bvs = []
-    for i in range(elements):
-        name = "unknown_bv_{row}_{col}".format(row=i%n, col = floor(i/n))
-        bv = BitVec(name, bits)
-        unknown_bvs.append(bv)
+            known_bvs = []
+            for i in range(elements):
+                # name = f"known_bv_{row}_{col}".format(row=i%n, col = floor(i/n))
+                bv = BitVecVal(i, bits)
+                known_bvs.append(bv)
 
-    known_bvs = []
-    for i in range(elements):
-        # name = f"known_bv_{row}_{col}".format(row=i%n, col = floor(i/n))
-        bv = BitVecVal(i, bits)
-        known_bvs.append(bv)
+            s = Solver();
+            s.set("timeout", 1000000)
 
-    s = Solver();
-    s.set("timeout", 1000000)
+            s.add(Distinct(*unknown_bvs))
 
-    s.add(Distinct(*unknown_bvs))
+            # Hamming Weight and Hamming Distance check
+            for i in range(len(known_bvs)):
+                s.add(Hamming_Weight(known_bvs[i] + unknown_bvs[i]) == weight)
+                s.add(Hamming_Distance(known_bvs[i], unknown_bvs[i]) == distance)
 
-    # Hamming Weight check
-    for i in range(len(known_bvs)):
-        s.add(Hamming_Weight(known_bvs[i] + unknown_bvs[i]) == weight)
-        s.add(Hamming_Distance(known_bvs[i], unknown_bvs[i]) == distance)
-
-    if(s.check() == sat):
-        print("YAY! Constraints Satisified In %d Bits" %(bits)),
-        m = s.model()
-        for d in m.decls():
-            print('\t{state}\t->\t{encode:0{fieldsize}b}'.format(state=d.name(),encode=m[d].as_long(),fieldsize=bits))
-    else:
-        print("Constraints Not Satisified in %d Bits" %(bits))
-        sys.stdout.flush()
+            if(s.check() == sat):
+                print("Constraints Satisified for HW: %d and HD: %d" %(weight, distance)),
+                m = s.model()
+                for d in m.decls():
+                    print('\t{state}\t->\t{encode:0{fieldsize}b}'.format(state=d.name(),encode=m[d].as_long(),fieldsize=bits))
+            else:
+                print("Constraints Not Satisified for HW: %d and HD: %d" %(weight, distance))
+                sys.stdout.flush()
     return
 
 def main():
-    n_by_n_s_box_unknown(n=2, weight=1, distance=1)
+    for n in range(2, 17):
+        n_by_n_s_box_unknown(n)
     return
 
 if __name__ == "__main__":
