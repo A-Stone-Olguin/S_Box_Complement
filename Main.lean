8db14a5ed931056cf16414182dcd64204320398a
Stone Olguin <olguinstone@gmail.com>
2023-11-06 13:48:52 -0700

    Started work on using Lean for Bitvecs

--- Main.lean
+++ Main.lean
@@ -1,4 +1,9 @@
-import «SBoxComplement»
+import Mathlib.Data.Bitvec.Defs
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+#eval (7 : Bitvec 3)
+
+instance (n : ℕ) : OfNat (Bitvec n) x where
+  ofNat := Bitvec.ofNat n x
+
+#eval (7 : Bitvec 3)
+#eval Bitvec.ofNat 3 7
