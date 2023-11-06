984cafb066f28a030e80bb55c4d5274510de105d
Stone Olguin <olguinstone@gmail.com>
2023-11-06 14:26:26 -0700

    Started progress on Lean verification

--- Main.lean
+++ Main.lean
@@ -20,3 +20,27 @@ def HammingWeight: ∀ {n : ℕ}, Bitvec n → ℕ
 
 #eval HammingWeight (4 : Bitvec 3) -- 1
 #eval HammingWeight (7 : Bitvec 3) -- 3
+#eval HammingWeight ((7 : Bitvec 3) + (4 : Bitvec 3)) -- 2
+#eval HammingWeight (Bitvec.one 3) -- 2
+
+-- #check cases
+
+lemma n_const_hamming_weight : ∀ {n : ℕ} (x : Bitvec n), ∃ (y : Bitvec n), HammingWeight (x + y) = n := by
+  intro n x
+  let y := -(x + Bitvec.one n)
+  use y
+  have h : x + -(x + Bitvec.one n ) = -(Bitvec.one n) := by
+    sorry
+  simp [y]
+  have h_calc : (-(Bitvec.one n)).toNat = 2^n - 1 := by
+    sorry
+
+
+  induction n with
+  | zero =>
+    have h : x + -(x + Bitvec.one zero ) = Bitvec.one zero := by simp
+    rw [h]
+    rfl
+  | succ n ih =>
+
+    sorry
