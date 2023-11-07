3dacd9588e79ccf3148458d6a4b4207742cc1e24
Stone Olguin <aolguin1@uwyo.edu>
2023-11-07 08:47:37 -0700

    Progress on proof, need to show "-1 = n-1 mod n"

--- Main.lean
+++ Main.lean
@@ -1,6 +1,7 @@
 import Mathlib.Data.Bitvec.Defs
+import Mathlib.Data.Bitvec.Lemmas
+import Mathlib.Tactic
 
--- #eval (7 : Bitvec 3)
 open Nat
 open Vector
 
@@ -21,26 +22,91 @@ def HammingWeight: ∀ {n : ℕ}, Bitvec n → ℕ
 #eval HammingWeight (4 : Bitvec 3) -- 1
 #eval HammingWeight (7 : Bitvec 3) -- 3
 #eval HammingWeight ((7 : Bitvec 3) + (4 : Bitvec 3)) -- 2
-#eval HammingWeight (Bitvec.one 3) -- 2
+#eval HammingWeight (-Bitvec.one 3) -- 3 (-1 = 7 mod 2^3)
 
 -- #check cases
+#check Bitvec.instAddBitvec
 
-lemma n_const_hamming_weight : ∀ {n : ℕ} (x : Bitvec n), ∃ (y : Bitvec n), HammingWeight (x + y) = n := by
+
+example : ∀ (n : ℕ), 2 ^(n+1) = 2 * 2^n := by apply?
+
+def neg_one : ∀ n : ℕ, Bitvec n
+  | 0 => nil
+  | succ n =>  replicate (succ n) true
+
+-- lemma append_true_succ_n : ∀ {n : ℕ}, true ::ᵥ -Bitvec.one n = -Bitvec.one (succ n) := by
+--   intro n
+--   induction n with
+--   | zero => rfl
+--   | succ n ih =>
+--     rw [← ih]
+
+
+--     sorry
+
+-- lemma pos_n_neg_one_all_ones : ∀ {n : ℕ}, -Bitvec.one n = (replicate n true) := by
+--   intro n
+--   induction n with
+--   | zero => rfl
+--   | succ n ih =>
+--     rw [replicate_succ true]
+
+--     sorry
+
+lemma neg_one_eq_neg_bitvec_one : ∀ {n : ℕ}, neg_one n = -Bitvec.one n := by
+  intro n
+  -- ext h
+  induction n with
+  | zero => rfl
+  | succ n ih =>
+    sorry
+
+lemma all_ones_hamming_weight_eq_n : ∀ {n : ℕ }, HammingWeight (replicate n true) = n :=  by
+  intro n
+  induction n with
+  | zero => rfl
+  | succ n ih =>
+    rw [replicate_succ true]
+    unfold HammingWeight
+    simp
+    rw [ih]
+    exact one_add n
+
+lemma neg_one_hamming_weight_eq_n : ∀ {n : ℕ}, HammingWeight (-Bitvec.one n) = n := by
+  intro n
+  rw [← neg_one_eq_neg_bitvec_one]
+  unfold HammingWeight
+  cases n with
+  | zero => rfl
+  | succ n =>
+    rw [neg_one]
+    simp
+    rw [@all_ones_hamming_weight_eq_n n]
+    exact one_add n
+
+#check (7 : Bitvec 3).toFin
+
+lemma neg_one_eq_two_pow_n_sub_one : ∀ {n : ℕ}, (-Bitvec.one n).toNat = 2^n - 1 := by
+  intro n
+  induction n with
+  | zero => rfl
+  | succ n ih =>
+    simp only [Bitvec.bitsToNat_toList, Vector.toList_singleton, Vector.head_cons]
+    unfold Bitvec.bitsToNat Bitvec.addLsb List.foldl
+
+  -- unfold bitsToNat addLsb List.foldl
+    sorry
+
+
+lemma const_hamming_weight_n : ∀ {n : ℕ} (x : Bitvec n), ∃ (y : Bitvec n), HammingWeight (x + y) = n := by
   intro n x
   let y := -(x + Bitvec.one n)
   use y
   have h : x + -(x + Bitvec.one n ) = -(Bitvec.one n) := by
+    have h1 : -(x + Bitvec.one n) = -x - Bitvec.one n := by sorry
+    rw [h1]
+
     sorry
   simp [y]
-  have h_calc : (-(Bitvec.one n)).toNat = 2^n - 1 := by
-    sorry
-
-
-  induction n with
-  | zero =>
-    have h : x + -(x + Bitvec.one zero ) = Bitvec.one zero := by simp
-    rw [h]
-    rfl
-  | succ n ih =>
-
-    sorry
+  rw [h]
+  exact neg_one_hamming_weight_eq_n
