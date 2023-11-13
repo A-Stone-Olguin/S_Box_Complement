43e7907f05b62e8b9fe5140b993b2a586753b472
Stone Olguin <olguinstone@gmail.com>
2023-11-13 14:24:31 -0700

    Added flipping functionality for Bitvec

--- Main.lean
+++ Main.lean
@@ -2,6 +2,8 @@ import Mathlib.Data.Bitvec.Defs
 import Mathlib.Data.Bitvec.Lemmas
 import Mathlib.Tactic
 
+-- import SBoxComplement.GroupBitvec
+
 open Nat
 open Vector
 
@@ -22,44 +24,41 @@ def HammingWeight: ∀ {n : ℕ}, Bitvec n → ℕ
 #eval HammingWeight (4 : Bitvec 3) -- 1
 #eval HammingWeight (7 : Bitvec 3) -- 3
 #eval HammingWeight ((7 : Bitvec 3) + (4 : Bitvec 3)) -- 2
-#eval HammingWeight (-Bitvec.one 3) -- 3 (-1 = 7 mod 2^3)
+#eval HammingWeight (-Bitvec.one 3) -- 3 (-1 = 7 mod 2^3 = 111₂)
 
--- #check cases
-#check Bitvec.instAddBitvec
+def HammingDistance : ∀ {n : ℕ}, Bitvec n → Bitvec n → ℕ :=
+  λ a b => HammingWeight (Bitvec.xor a b)
+#eval HammingDistance (4 : Bitvec 3) (7 : Bitvec 3) -- 100 ⊕ 111 = 011, 2 HD
+
+def Vector.modifyNth {α: Type} (n : ℕ) (f : α → α): ℕ → Vector α n → Vector α n:=
+  λ nth v => ⟨List.modifyNth f nth v.val, by simp [*]⟩
+
+-- Modifies the nth element of a bitvector
+def Bitvec.modifyNth (n : ℕ) (f : Bool → Bool): ℕ → Bitvec n → Bitvec n :=
+  λ nth b => Vector.modifyNth n f nth b
+
+-- Flips the Bitvector's values at each specified place in the list
+def Bitvec.flip : ∀ {n : ℕ}, (List ℕ) × (Bitvec n) → Bitvec n
+  | _, ([], b) => b
+  | n, (a :: as, b) => Bitvec.flip (as, Bitvec.modifyNth n Bool.not a b)
+#eval (7 : Bitvec 3)                          --111
+#eval Bitvec.flip ([0, 2], (7 : Bitvec 3))    --010
+
+def first_n_nats : ℕ → List ℕ
+  | 0 => []
+  | n + 1 => (n :: (first_n_nats n).reverse).reverse
+#eval first_n_nats 5
+
+def first_n_even_nats : ℕ → List ℕ :=
+  λ n => List.map (λ (x : Nat) => 2 * x) (first_n_nats n)
+#eval first_n_even_nats 4
+
+def flipped_complement : Bitvec n → ℕ → Bitvec n :=
+  λ b hw => Bitvec.flip (List.diff (first_n_nats n) (first_n_even_nats (n-hw)), b)
+#eval (32 : Bitvec 8)                             --00100000
+#eval flipped_complement (32 : Bitvec 8) 7        --01011111
 
 
-example : ∀ (n : ℕ), 2 ^(n+1) = 2 * 2^n := by apply?
-
-def neg_one : ∀ n : ℕ, Bitvec n
-  | 0 => nil
-  | succ n =>  replicate (succ n) true
-
--- lemma append_true_succ_n : ∀ {n : ℕ}, true ::ᵥ -Bitvec.one n = -Bitvec.one (succ n) := by
---   intro n
---   induction n with
---   | zero => rfl
---   | succ n ih =>
---     rw [← ih]
-
-
---     sorry
-
--- lemma pos_n_neg_one_all_ones : ∀ {n : ℕ}, -Bitvec.one n = (replicate n true) := by
---   intro n
---   induction n with
---   | zero => rfl
---   | succ n ih =>
---     rw [replicate_succ true]
-
---     sorry
-
-lemma neg_one_eq_neg_bitvec_one : ∀ {n : ℕ}, neg_one n = -Bitvec.one n := by
-  intro n
-  -- ext h
-  induction n with
-  | zero => rfl
-  | succ n ih =>
-    sorry
 
 lemma all_ones_hamming_weight_eq_n : ∀ {n : ℕ }, HammingWeight (replicate n true) = n :=  by
   intro n
@@ -72,41 +71,16 @@ lemma all_ones_hamming_weight_eq_n : ∀ {n : ℕ }, HammingWeight (replicate n 
     rw [ih]
     exact one_add n
 
-lemma neg_one_hamming_weight_eq_n : ∀ {n : ℕ}, HammingWeight (-Bitvec.one n) = n := by
-  intro n
-  rw [← neg_one_eq_neg_bitvec_one]
-  unfold HammingWeight
-  cases n with
-  | zero => rfl
-  | succ n =>
-    rw [neg_one]
-    simp
-    rw [@all_ones_hamming_weight_eq_n n]
-    exact one_add n
-
-#check (7 : Bitvec 3).toFin
-
-lemma neg_one_eq_two_pow_n_sub_one : ∀ {n : ℕ}, (-Bitvec.one n).toNat = 2^n - 1 := by
-  intro n
-  induction n with
-  | zero => rfl
-  | succ n ih =>
-    simp only [Bitvec.bitsToNat_toList, Vector.toList_singleton, Vector.head_cons]
-    unfold Bitvec.bitsToNat Bitvec.addLsb List.foldl
-
-  -- unfold bitsToNat addLsb List.foldl
-    sorry
-
-
 lemma const_hamming_weight_n : ∀ {n : ℕ} (x : Bitvec n), ∃ (y : Bitvec n), HammingWeight (x + y) = n := by
   intro n x
-  let y := -(x + Bitvec.one n)
-  use y
-  have h : x + -(x + Bitvec.one n ) = -(Bitvec.one n) := by
-    have h1 : -(x + Bitvec.one n) = -x - Bitvec.one n := by sorry
-    rw [h1]
-
-    sorry
-  simp [y]
+  let y := -x + replicate n true
+  use -x + replicate n true
+  have h : x + (-x + replicate n true) = replicate n true := by
+    have h_assoc : x + (-x + replicate n true) = x + -x + replicate n true := by sorry
+    rw [h_assoc]
+    have h_inv : x + -x = Bitvec.zero n := by sorry
+    rw [h_inv]
+    have h_add_zero : Bitvec.zero n + replicate n true = replicate n true := by sorry
+    rw [h_add_zero]
   rw [h]
-  exact neg_one_hamming_weight_eq_n
+  exact all_ones_hamming_weight_eq_n
